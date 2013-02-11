39d154545f00b66f100b372e8a006d818388f124
Evan Hensleigh <futuraprime@gmail.com>
2013-02-10 23:33:46 -0500

    Added Backbone Relational.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,13 +2,18 @@ requirejs.config({
     baseUrl: '/javascripts/app/',
     paths : {
         'tmpl' : '../../templates',
-        'backbone' : '../lib/backbone',
+        'backbone-core' : '../lib/backbone',
+        'backbone' : '../lib/backbone-relational',
         'underscore' : '../lib/underscore-min',
         'jade' : '../lib/jade',
         'jquery' : '../lib/jquery-1.9.1.min'
     },
     shim : {
         'backbone' : {
+            deps: ['backbone-core'],
+            exports : 'Backbone'
+        },
+        'backbone-core' : {
             deps: ['underscore', 'jquery'],
             exports : 'Backbone'
         },
